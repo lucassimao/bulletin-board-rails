5631db8ed0c64e95d1f5242f94f791070a5209e0
Lucas Simão <lsimaocosta@gmail.com>
2020-01-28 12:30:16 -0300

    ensuring assigning profiles to user is working

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,36 @@
-require 'test_helper'
+require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be able to asign the profile super_admin" do
+    u = User.new(password: '123', email: 'u1@gmail.com')
+
+    assert_not u.super_admin?
+    u.super_admin = true
+
+    assert u.super_admin?
+    u.save!
+
+    assert_equal User.find(u.id).profiles, ['super_admin']
+  end
+
+  test "should be able to assign the profile organization_admin" do
+    u = User.new(password: '123', email: 'u1@gmail.com')
+    assert_not u.organization_admin?
+    u.organization_admin = true
+    assert u.organization_admin?
+
+    u.save!
+    assert_equal User.find(u.id).profiles, ['organization_admin']
+  end
+
+  test "should be able to assign the profile user" do
+    u = User.new(password: '123', email: 'u1@gmail.com')
+
+    assert_not u.user?
+    u.user = true
+    assert u.user?
+
+    u.save!
+    assert_equal User.find(u.id).profiles, ['user']
+  end
 end
