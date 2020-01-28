e351b0efba8dba7e7724d58037e33d08348b082b
Lucas Simão <lsimaocosta@gmail.com>
2020-01-28 17:15:26 -0300

    WIP organization dashboard

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -3,9 +3,9 @@ class OrganizationsController < ApplicationController
 
   # GET /organizations
   # GET /organizations.json
-  def index
-    @organizations = Organization.all
-  end
+#   def index
+#     @organization = Organization.get(session[:organization_id])
+#   end
 
   # GET /organizations/1
   # GET /organizations/1.json
