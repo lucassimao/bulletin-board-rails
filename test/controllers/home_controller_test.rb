a9c874a1b817198a1e30abef7058be92c99d40a8
Lucas Simão <lsimaocosta@gmail.com>
2020-01-28 12:46:52 -0300

    ensuring the /signup route is properly configured

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,14 @@
-require 'test_helper'
+require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get home_index_url
+    get root_url
     assert_response :success
   end
 
+  test "shoudl get the signup page" do
+    assert_routing '/signup', controller: 'organizations', action: 'new'
+    get signup_url
+    assert_response :success
+  end
 end
