4bb5253c0bad0cb9117e7ae116e807ab42630fe2
Lucas Simão <lsimaocosta@gmail.com>
2020-01-29 18:15:42 -0300

    WIP dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+    before_action :authorize, except: [:signout]
+
+    def signout
+        session[:current_user_id] = nil
+        redirect_to :root
+    end
+
+    protected
+    def authorize
+        @current_user = Organization.find(3).admin
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root "home#index"
   
+  get 'signout', to: 'home#signout'
   get 'signup', to: 'organizations#new'
-  resources :organizations
+  get 'dashboard', to: 'organizations#index'
+#   resources :organizations
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
