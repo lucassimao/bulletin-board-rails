7800ba25b979c0a83e8b6a5e5fa1c34105060f51
Lucas Simão <lsimaocosta@gmail.com>
2020-01-30 16:49:08 -0300

    exposing organizations#create

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'signout', to: 'home#signout'
   get 'signup', to: 'organizations#new'
   get 'dashboard', to: 'organizations#index'
-#   resources :organizations
+  resources :organizations, only: [:create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
