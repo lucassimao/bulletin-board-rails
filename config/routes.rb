df13bb1db828ff5ee39a945271604b0d09fedfb6
Lucas Simão <lsimaocosta@gmail.com>
2020-01-30 16:50:14 -0300

    renaming /dashboard to /board

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   
   get 'signout', to: 'home#signout'
   get 'signup', to: 'organizations#new'
-  get 'dashboard', to: 'organizations#index'
+  get 'board', to: 'organizations#index'
   resources :organizations, only: [:create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
